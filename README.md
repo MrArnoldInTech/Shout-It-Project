f60bf2ada22e684df1f5c8892aef87ae4b6dcff3
TheonlyArnold <arniefruish@yahoo.co.nz>
2024-08-13 22:53:29 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shout-It-Project
+ Php basic project with lobby text with users
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html> 
+    <head>
+        <meta charset="utf-8" />
+        <title>SHOUT IT!</title>
+        <link rel="stylesheet" href="css/style.css" type "tesxt/css" />
+</head> 
+<body>
+ <div id="container">
+    <header>
+        <h1> SHOUT IT! Shoutbox</h1>
+</header>
+<div id="shouts">
+    <ul>
+        <li class="shout"><span>11:01PM - </span>Bob: Hey hows it going? </li>
+        <li class="shout"><span>11:01PM - </span>Bob: Hey hows it going? </li>
+        <li class="shout"><span>11:01PM - </span>Bob: Hey hows it going? </li>
+        <li class="shout"><span>11:01PM - </span>Bob: Hey hows it going? </li>
+        <li class="shout"><span>11:01PM - </span>Bob: Hey hows it going? </li>
+        </ul>
+    </div>
+            <div id="input">
+                <form method="post" action="process.php">
+                    <input type="text" name="user" palceholder="Enter your Name" />
+                    <input type="text" name="user" palceholder="Enter your Message" />
+                    <br />
+                    <input class="shout-btn" name="submit" value="Shout It Out" />
+                </form>
+
+
+            </div>
+
+        </div>
+   </body>
+</html>
+
+
